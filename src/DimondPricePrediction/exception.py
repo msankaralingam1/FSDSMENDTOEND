4661a3965fa3de91863314dabf13f041fa208fe6
Your Name~ <you@example.com>
2023-10-31 14:10:06 +0530

    exception update

--- src/DimondPricePrediction/exception.py
+++ src/DimondPricePrediction/exception.py
@@ -4,7 +4,7 @@ import sys
 class customexception(Exception):
     def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
+        _,_,exc_tb = error_details.exc_info()  # exc_tb = excequition trace back.
         
         self.lineno=exc_tb.tb_lineno
         self.file_name=exc_tb.tb_frame.f_code.co_filename 
